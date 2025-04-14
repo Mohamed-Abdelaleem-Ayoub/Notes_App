02fa4876076e5cdcb9aae5a34d4388f72747b860
Mohamed-Abdelaleem-Ayoub <ma5737@fayoum.edu.ed>
2025-04-14 18:25:22 +0200

    create notes list view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/custom_note_item.dart';
+import 'package:notes_app/widgets/notes_list_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [CustomAppBar(), CustomNoteItem()]));
+    return Scaffold(body: Column(children: [CustomAppBar(), NotesListView()]));
   }
 }
